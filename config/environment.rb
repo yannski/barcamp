2011af615b093ed31fd2082fa02545b08a71aa30
Yann Klis <yann.klis@novelys.com>
2009-11-23 11:57:29 +0100

    Upgraded to Rails 2.3.4

--- config/environment.rb
+++ config/environment.rb
@@ -1,7 +1,7 @@
 # Be sure to restart your server when you modify this file
 
 # Specifies gem version of Rails to use when vendor/rails is not present
-RAILS_GEM_VERSION = '2.3.2' unless defined? RAILS_GEM_VERSION
+RAILS_GEM_VERSION = '2.3.4' unless defined? RAILS_GEM_VERSION
 
 # Bootstrap the Rails environment, frameworks, and default configuration
 require File.join(File.dirname(__FILE__), 'boot')
@@ -12,6 +12,5 @@ Rails::Initializer.run do |config|
   config.gem 'haml'
   config.gem 'RedCloth'
   config.gem 'authlogic'
-  config.gem 'thoughtbot-paperclip',        :lib => 'paperclip',        :source => "http://gems.github.com"
-  config.gem 'thoughtbot-hoptoad_notifier', :lib => 'hoptoad_notifier', :source => 'http://gems.github.com'
+  config.gem 'paperclip'
 end
