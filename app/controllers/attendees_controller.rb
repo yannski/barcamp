f9f31562c5ab12a819848e468d51347eb0b9d762
Yann Klis <yann.klis@novelys.com>
2009-11-27 11:41:54 +0100

    CSV mime type for attendees listing

--- app/controllers/attendees_controller.rb
+++ app/controllers/attendees_controller.rb
@@ -2,6 +2,27 @@ class AttendeesController < ApplicationController
 
   def index
     @attendees = Attendee.all
+
+    respond_to do |format|
+      format.html
+      format.csv {
+        csv_string = FasterCSV.generate do |csv|
+          columns = %w(firstname lastname email organization website tshirt_size)
+          # header row
+          csv << columns
+          
+          # data rows
+          @attendees.each do |attendee|
+            csv << columns.map{|column| attendee.send column.to_sym}
+          end
+        end
+        
+        # send it to the browsah
+        send_data csv_string,
+          :type => 'text/csv; charset=iso-8859-1; header=present',
+          :disposition => "attachment; filename=attendees.csv"
+      }
+    end
   end
 
   def new
