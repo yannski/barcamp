97ee34ac559d72db415ac60efc2c91540c68b6d1
Yann Klis <yann.klis@novelys.com>
2010-03-13 16:59:56 +0100

    Completed translations

--- config/routes.rb
+++ config/routes.rb
@@ -1,5 +1,5 @@
 ActionController::Routing::Routes.draw do |map|
-  Translate::Routes.translation_ui(map)
+  Translate::Routes.translation_ui(map) if Rails.env != "Production"
   map.filter 'locale'
 
   map.root :controller => "welcome"
