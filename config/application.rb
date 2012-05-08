230d0689b9e836f0086451dc6fd8b9aba40e1241
Yann Klis <yann.klis@novelys.com>
2012-05-09 01:19:51 +0200

    do not initialize when precompiling assets

--- config/application.rb
+++ config/application.rb
@@ -61,6 +61,8 @@ module Barcamp
     # Enable the asset pipeline
     config.assets.enabled = true
 
+    config.assets.initialize_on_precompile = false
+
     # Version of your assets, change this if you want to expire all your assets
     config.assets.version = '1.0'
   end
