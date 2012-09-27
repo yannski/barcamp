f5507f8063cd06a46c0d4365649ebab124ff5479
Yann Klis <yann.klis@novelys.com>
2012-09-27 16:07:44 +0200

    socialgoodweek alterations

--- config/application.rb
+++ config/application.rb
@@ -38,8 +38,8 @@ module Barcamp
     # The default locale is :en and all translations from config/locales/*.rb,yml are auto loaded.
     # config.i18n.load_path += Dir[Rails.root.join('my', 'locales', '*.{rb,yml}').to_s]
     # config.i18n.default_locale = :de
-    config.i18n.available_locales = [:en , :fr, :de]
-    config.i18n.default_locale = :en
+    config.i18n.available_locales = [:fr]
+    config.i18n.default_locale = :fr
 
     # Configure the default encoding used in templates for Ruby 1.9.
     config.encoding = "utf-8"
